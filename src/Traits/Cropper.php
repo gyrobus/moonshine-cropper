e2c352084db131d4e1bbe12ffc33191212aacef5
Andante <pavel-andante@yandex.ru>
2025-04-09 22:29:24 +0300

    Change Cropper trait

--- src/Traits/Cropper.php
+++ src/Traits/Cropper.php
@@ -6,8 +6,6 @@ use Illuminate\Support\Facades\Storage;
 
 trait Cropper
 {
-    protected string $cropperDisk;
-
     public function cropImage($name, $disk = null)
     {
         return Storage::disk($disk ?? ($this->cropperDisk ?? config('moonshine.disk')))
